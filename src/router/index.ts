04bef71fd604c694d45b89ce74a13a186765a900
ljylwnxx <ljylwnxx@163.com>
2023-05-10 10:44:51 +0800

    chore:配置路由

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        redirect: '/home',
+            children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: () => import('@/views/home/index.vue'),
+                meta: { title: '首页'},
+            },
+            ],
+    },
+    // {
+    //     path: '/index',
+    //     component: () => import('@/views/layout/index.vue'),
+    // }
+]
+const router =createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+export default router
